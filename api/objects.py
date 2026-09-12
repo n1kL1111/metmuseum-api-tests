c8f62f982cf8b16bed98499a9575d99034a36a33
NiktiaL <n1kl.1337.03@gmail.com>
2026-09-12 07:59:17 +0500

    Allure add

--- api/objects.py
+++ api/objects.py
@@ -1,3 +1,4 @@
+import allure
 from requests import Response
 
 from .client import ApiClient
@@ -7,8 +8,10 @@ class ObjectsApi:
     def __init__(self, client: ApiClient):
         self.client = client
 
+    @allure.step("Получить произведение искусства по ID: {object_id}")
     def get_object(self, object_id: int) -> Response:
         return self.client.get(f"objects/{object_id}")
 
+    @allure.step("Получить список произведений искусства")
     def get_objects(self) -> Response:
         return self.client.get("objects")
