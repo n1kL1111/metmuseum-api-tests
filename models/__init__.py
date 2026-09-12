0b500fdabc24a60a5500527d0be2d520bdb5a867
NiktiaL <n1kl.1337.03@gmail.com>
2026-09-12 06:05:14 +0500

    Structure refactoring

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,8 @@
 from .artwork import Artwork
-from .constituent import Constituent
-from .measurement import Measurement
 from .object_list import ObjectList
-from .tag import Tag
 
 __all__ = [
     "Artwork",
-    "Constituent",
-    "Measurement",
     "ObjectList",
-    "Tag",
 ]
 
